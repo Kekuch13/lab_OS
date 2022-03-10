23ae882efb1bdda6a9a0efe371668ccb0d98d9c0
Matvey Emelyanov <98802601+Kekuch13@users.noreply.github.com>
2022-03-10 12:50:48 +0400

    Upload lab3_1

---
+++ lab3/lab3_1.cpp
@@ -0,0 +1,66 @@
+#include <cstdio>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int fd[2];
+
+void *proc1(void *flag)
+{
+    int *flag1 = (int *)flag;
+    printf("Thread 1 has started working\n");
+
+    dev_t devID;
+    struct stat buff;
+    while (*flag1 != 1)
+    {        
+        stat("lab3_1.cpp", &buff);
+        devID = buff.st_dev;
+        write(fd[1], &devID, sizeof(dev_t));
+        sleep(1);
+    }
+    printf("Thread 1 has finished working\n");
+    pthread_exit((void *)33);
+}
+
+void *proc2(void *flag)
+{
+    int *flag2 = (int *)flag;
+    printf("Thread 2 has started working\n");
+
+    dev_t devID;
+    while (*flag2 != 1)
+    { 
+        devID = (dev_t) 0;
+        read(fd[0], &devID, sizeof(dev_t));
+        printf("ID of device containing file: %lu\n", devID);
+    }
+    printf("Thread 2 has finished working\n");
+    pthread_exit((void *)44);
+}
+
+int main()
+{
+    pthread_t id1, id2;
+    int flag1 = 0, flag2 = 0;
+    int exitcode1, exitcode2;
+
+    pipe(fd);
+    pthread_create(&id1, NULL, proc1, &flag1);
+    usleep(1);
+    pthread_create(&id2, NULL, proc2, &flag2);
+    
+    printf("Program is waiting for the key to be pressed\n\n");
+    getchar();
+    printf("Key has been pressed\n\n");
+
+    flag1 = flag2 = 1;
+    pthread_join(id1, (void**) &exitcode1);
+    pthread_join(id2, (void**) &exitcode2);
+    printf("\nThread 1 exitcode = %d\n", exitcode1);
+    printf("Thread 2 exitcode = %d\n", exitcode2);
+
+    close(fd[0]);
+    close(fd[1]);
+    return 0;
+}
