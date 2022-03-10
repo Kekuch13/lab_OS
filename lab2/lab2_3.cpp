b7a065f46318bc394ffd39c058eff6e6d93b085a
Matvey Emelyanov <98802601+Kekuch13@users.noreply.github.com>
2022-03-10 12:43:24 +0400

    Upload lab2_3

---
+++ lab2/lab2_3.cpp
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <cstring>
+#include <errno.h>
+
+pthread_mutex_t mut;
+
+void *proc1(void *flag)
+{
+    int *flag1 = (int *)flag;
+    printf("Thread 1 has started working\n");
+    while (*flag1 != 1)
+    {
+        timespec tp;
+        clock_gettime(CLOCK_REALTIME, &tp);
+        tp.tv_sec += 2;
+        int rv = pthread_mutex_timedlock(&mut, &tp);
+        if (rv == 0)
+        {
+            for (int i = 0; i < 5; ++i)
+            {
+                putchar('1');
+                fflush(stdout);
+                sleep(1);
+            }
+            pthread_mutex_unlock(&mut);
+            sleep(1);
+        }
+        else
+        {
+            printf("\nThread 1: pthread_mutex_timedlock() error: %s\n", std::strerror(rv));
+        }
+    }
+    printf("Thread 1 has finished working\n");
+    pthread_exit((void *)33);
+}
+
+void *proc2(void *flag)
+{
+    int *flag2 = (int *)flag;
+    printf("Thread 2 has started working\n");
+    while (*flag2 != 1)
+    {
+        timespec tp;
+        clock_gettime(CLOCK_REALTIME, &tp);
+        tp.tv_sec += 2;
+        int rv = pthread_mutex_timedlock(&mut, &tp);
+        if (rv == 0)
+        {
+            for (int i = 0; i < 5; ++i)
+            {
+                putchar('2');
+                fflush(stdout);
+                sleep(1);
+            }
+            pthread_mutex_unlock(&mut);
+            sleep(1);
+        }
+        else
+        {
+            printf("\nThread 2: pthread_mutex_timedlock() error: %s\n", std::strerror(rv));
+        }
+    }
+    printf("Thread 2 has finished working\n");
+    pthread_exit((void *)44);
+}
+
+int main()
+{
+    pthread_t id1, id2;
+    int flag1 = 0, flag2 = 0;
+    int exitcode1, exitcode2;
+
+    pthread_mutex_init(&mut, NULL);
+    pthread_create(&id1, NULL, proc1, &flag1);
+    pthread_create(&id2, NULL, proc2, &flag2);
+
+    printf("Program is waiting for the key to be pressed\n\n");
+    getchar();
+    printf("Key has been pressed\n\n");
+
+    flag1 = flag2 = 1;
+    pthread_join(id1, (void**) &exitcode1);
+    pthread_join(id2, (void**) &exitcode2);
+
+    printf("\nThread 1 exitcode = %d\n", exitcode1);
+    printf("Thread 2 exitcode = %d\n", exitcode2);
+
+    pthread_mutex_destroy(&mut);
+    return 0;
+}
